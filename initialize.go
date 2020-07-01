2e5e8e5b4dc8aaa0c64c5acf16560f6118d6ebe0
vivevincere <s.vincent@live.com.sg>
2020-07-02 01:18:18 +0800

    fixed some errors

--- initialize.go
+++ initialize.go
@@ -14,23 +14,23 @@ type SdkObjects struct{
 }
 
 func NewKey(id string, timestamp int64)[][]byte{
-return [][]byte{[]byte(id),[]byte(timestamp)}
+return [][]byte{[]byte(id),timestamp)}
 }
 
 
-func NewPayload(id string, timestamp int64, goosePacket string){
-return [][]byte{[]byte(id),[]byte(timestamp), []byte(goosePacket)}
+func NewPayload(id string, timestamp int64, goosePacket string) [][]byte{
+return [][]byte{[]byte(id),timestamp, []byte(goosePacket)}
 
 }
 
 func Init(configFile string, channelID string, user string, org string) SdkObjects{
 	var SdkObjects toReturn
-	toReturn.sdk, err := fabsdk.New(config.FromFile(configFile))
+	toReturn.sdk, err = fabsdk.New(config.FromFile(configFile))
 	if err != nil {
 		log.Fatal("failed to create sdk: %v", err)
 	}
 	clientContext := sdk.ChannelContext(channelID,fabsdk.WithUser(user), fabsdk.WithOrg(org))
-	toReturn.client, err := channel.New(clientContext)
+	toReturn.client, err = channel.New(clientContext)
 	if err != nil {
 		log.Fatal("Failed to create new channel: %v", err)
 	}
@@ -41,7 +41,7 @@ func (thesdk SdkObjects) Set( chaincodeID string, id string, timestamp int64, go
 	defArgs := NewPayload(id, timestamp, goosePacket)
 	response, err := thesdk.client.Execute(channel.Request{ChaincodeID: chaincodeID, Fcn: "LogEvent", Args: defArgs})
 	if err != nil{
-		return error	
+		return err	
 	}
 	return  nil
 }
